9d8589e6207be1f3d04280332cb02ba2cd68b3db
Francesco Minnocci <ad17fmin@uwcad.it>
2020-08-18 16:41:56 +0200

    Improve font; colors

--- config.def.h
+++ config.def.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "monospace:size=9";
-static const char* normbgcolor  = "#222222";
-static const char* normfgcolor  = "#cccccc";
+static const char font[]        = "monospace:size=10";
+static const char* normbgcolor  = "#282828";
+static const char* normfgcolor  = "#ebdbb2";
 static const char* selbgcolor   = "#555555";
 static const char* selfgcolor   = "#ffffff";
-static const char* urgbgcolor   = "#111111";
+static const char* urgbgcolor   = "#1d2021";
 static const char* urgfgcolor   = "#cc0000";
 static const char before[]      = "<";
 static const char after[]       = ">";
